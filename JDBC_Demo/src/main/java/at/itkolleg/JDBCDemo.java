8dfe759ae20851bbec78003b7ada4c1066ce363b
sargosch <sargosch@tsn.at>
2022-11-23 11:21:06 +0100

    Demo-Projekt FindByName verarbeitet

--- JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
+++ JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
@@ -8,7 +8,7 @@ public class JDBCDemo {
         System.out.println();
         selectAllDemo();
         System.out.println();
-        insertStudentDemo("Günter Gosch", "guegosch@myimst.at");
+        //insertStudentDemo("Günter Gosch", "guegosch@myimst.at");
         System.out.println();
         selectAllDemo();
         System.out.println();
@@ -19,11 +19,31 @@ public class JDBCDemo {
         deleteStudentDemo(10);
         System.out.println();
         selectAllDemo();
+        findAllByNameLike("ma");
 
     }
 
-    public static void findAllByName(String name) {
+    public static void findAllByNameLike(String findName) {
+        System.out.println("FindByName Demo mit JDBC");
+        String sqlSelectAllPersons = "SELECT * FROM `student` WHERE `student`.`name` LIKE ?";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+        try (Connection con = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
 
+            PreparedStatement preparedStatement = con.prepareStatement(sqlSelectAllPersons);
+            preparedStatement.setString(1, "%" + findName + "%");
+            ResultSet rs =  preparedStatement.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println("Student aus der DB: [ID] " + id + " [NAME] " + name + " [EMAIL] " + email);
+            }
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: \n" + e.getMessage());
+        }
     }
 
     public static void deleteStudentDemo(int studentId) {
