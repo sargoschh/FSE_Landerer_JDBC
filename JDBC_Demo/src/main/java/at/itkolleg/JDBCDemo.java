b83d6319f64683c58e2d23f5e134c77131482dbd
sargosch <sargosch@tsn.at>
2022-11-23 09:53:32 +0100

    Demo-Projekt Verbindung DB hergestellt

--- JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
+++ JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
@@ -1,7 +1,26 @@
 package at.itkolleg;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCDemo {
     public static void main(String[] args) {
         System.out.println("JDBC Demo!");
+        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Sarah Gosch', 'sargosch@myimst.at'), (NULL, 'Marcel Schranz', 'marschranz@myimst.at');
+
+        selectAllDemo();
+
+    }
+
+    public static void selectAllDemo() {
+        System.out.println("Select Demo mit JDBC");
+        String sqlSelectAllPersons = "SELECT * FROM `student`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        try (Connection con = DriverManager.getConnection(connectionUrl, "root", "")){
+            System.out.println("Verbindung zur DB hergestellt!");
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
     }
 }
