714050669669a9db2d46c5252f15dfef3437e3c3
sargosch <sargosch@tsn.at>
2022-11-24 09:00:54 +0100

    Demo-Projekt CreateNewTable eingearbeitet

--- JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
+++ JDBC_Demo/src/main/java/at/itkolleg/JDBCDemo.java
@@ -20,7 +20,91 @@ public class JDBCDemo {
         System.out.println();
         selectAllDemo();
         findAllByNameLike("ma");
+        System.out.println();
+        createNewTable();
+        System.out.println();
+        insertKursDemo("POS", 15);
+        System.out.println();
+        selectAllFromKursDemo();
 
+    }
+
+    public static void createNewTable() {
+        System.out.println("Create new Table Demo mit JDBC");
+        String createNewTable = "CREATE TABLE `jdbcdemo`.`kurs` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(200) NOT NULL , `maxstudents` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+        try (Connection con = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = con.prepareStatement(createNewTable);
+            try {
+                int affectedRows = preparedStatement.executeUpdate();
+                System.out.println("Anzahl der erstellten Tabellen: " + affectedRows);
+            } catch (SQLException ex) {
+                System.out.println("Fehler beim Erstellen der Tabelle: \n" + ex.getMessage());
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: \n" + e.getMessage());
+        }
+    }
+
+    public static void insertKursDemo(String name, int maxStudents) {
+        System.out.println("Insert Demo mit JDBC");
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+        /*
+         * reagiert auf Verbindung mit Datenbank
+         *
+         * Schreibt man die Verbindung in den Head des try-Blocks,
+         * so kann man auf con.close verzichten, da die Verbindung
+         * automatisch geschlossen wird, sobald der try-Block abgearbeitet
+         * ist.
+         */
+        try (Connection con = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = con.prepareStatement(
+                    "INSERT INTO `kurs` (`id`, `name`, `maxstudents`) VALUES (NULL, ?, ?)");
+            //reagiert auf Absetzen des Statements
+            try {
+                preparedStatement.setString(1, name);
+                preparedStatement.setInt(2, maxStudents);
+                int rowAffected = preparedStatement.executeUpdate();
+                System.out.println(rowAffected + " Datensätze eingefügt");
+            }catch (SQLException ex) {
+                System.out.println("Fehler im SQL-INSERT Statement: " + ex.getMessage());
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: \n" + e.getMessage());
+        }
+    }
+
+    public static void selectAllFromKursDemo() {
+        System.out.println("Select Demo mit JDBC");
+        String sqlSelectAllPersons = "SELECT * FROM `kurs`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+        try (Connection con = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = con.prepareStatement(sqlSelectAllPersons);
+            ResultSet rs =  preparedStatement.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int maxstudents = rs.getInt("maxstudents");
+                System.out.println("Kurs aus der DB: [ID] " + id + " [NAME] " + name + " [MAXSTUDENTS] " + maxstudents);
+            }
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: \n" + e.getMessage());
+        }
     }
 
     public static void findAllByNameLike(String findName) {
