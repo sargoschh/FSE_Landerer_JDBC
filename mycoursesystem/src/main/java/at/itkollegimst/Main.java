4b69540abeb8e1ac6339563d8375caf2d8ae2355
sargoschh <sargosch@tsn.at>
2022-11-30 10:17:25 +0100

    JDBC/DAO Datenbankverbindung hergestellt

--- mycoursesystem/src/main/java/at/itkollegimst/Main.java
+++ mycoursesystem/src/main/java/at/itkollegimst/Main.java
@@ -1,7 +1,26 @@
 package at.itkollegimst;
 
+import dataaccess.MysqlDatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/kurssystem";
+        String user = "root";
+        String pwd = "";
+
+        try {
+            Connection myConnection = MysqlDatabaseConnection.getConnection(url, user, pwd);
+            System.out.println("Verbindung aufgebaut!");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
